c1a8466b2c993bacdfa5295e8acd04b24347d767
kusumajamana <kusumajamana03@gmail.com>
2025-02-03 12:05:19 +0530

    core functionality done

--- src/main/java/com/example/transaction_microservice/TransactionMicroserviceApplication.java
+++ src/main/java/com/example/transaction_microservice/TransactionMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.example.transaction_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TransactionMicroserviceApplication {
 
 	public static void main(String[] args) {
